a83f9576b584e30b6ff15a3a639078b3b7b57730
Mike <cyberenot@gmail.com>
2022-01-18 11:37:37 -0800

    Palindrome Number

---
+++ leetcode.playground/Pages/Palindrome Number.xcplaygroundpage/Contents.swift
@@ -0,0 +1,42 @@
+let num = 123
+
+// Bruteforce solution
+func isPalindrome(_ x: Int) -> Bool {
+    if x < 0 { return false }
+    let chars = String(x)
+    let reversedChars = String(chars.reversed())
+    
+    if chars == reversedChars {
+        return true
+    }
+    
+    return false
+}
+
+isPalindrome(num)
+
+
+// Efficient solution
+func isPalindrome2(_ x: Int) -> Bool {
+    guard x >= 0 else { return false }
+    
+    var x = x
+    var arr: [Int] = []
+    var arrInverted: [Int] = []
+    
+    while x != 0 {
+        let pop = x % 10
+        x = x / 10
+        
+        arr.insert(pop, at: 0)
+        arrInverted.insert(pop, at: arrInverted.endIndex)
+    }
+    
+    if arr == arrInverted {
+        return true
+    }
+    
+    return false
+}
+
+isPalindrome2(num)
