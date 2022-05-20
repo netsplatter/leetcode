d4645ef8612959796d46759faf3e1dcc9521cc66
Mike <mraccoondev@gmail.com>
2022-05-20 16:26:50 -0700

    fix TwoSum

--- leetcode.playground/Pages/Two Sum.xcplaygroundpage/Contents.swift
+++ leetcode.playground/Pages/Two Sum.xcplaygroundpage/Contents.swift
@@ -6,10 +6,8 @@ func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
     var dict = [Int: Int]()
     
     for (i, num) in nums.enumerated() {
-        if let index = dict[target-num] {
-            if i != index {
-                return [index, i]
-            }
+        if let index = dict[target - num] {
+            return [index, i]
         }
         dict[num] = i
     }
