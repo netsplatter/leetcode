6c60f617f163c4c6ce18d84854be3847f8750148
Mike <mraccoondev@gmail.com>
2022-06-27 16:02:29 -0700

    fix

--- leetcode.playground/Pages/Valid Palindrome.xcplaygroundpage/Contents.swift
+++ leetcode.playground/Pages/Valid Palindrome.xcplaygroundpage/Contents.swift
@@ -1,5 +1,6 @@
+// it's necessary to ask can the input string be empty or consist of 1 character
 func isPalindrome(_ s: String) -> Bool {
-    guard !s.isEmpty else { return true }
+    guard !s.isEmpty || s.count == 1 else { return true } // if the string can be empty or has 1 character, then we use this guard statement
     
     var cleanString = ""
     for char in s {
